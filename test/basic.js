50c6ca8670ba314c72cd59a4276072f1f8f58df2
Eajaz Ali Mahaboob Basha <eajazali.mahaboobbasha@pearson.com>
2016-01-29 23:09:10 -0700

    test

--- test/basic.js
+++ test/basic.js
@@ -4,6 +4,7 @@ var webdriver = require("selenium-webdriver");
 describe("testing javascript in the browser", function() {
   beforeEach(function() {
     if (process.env.SAUCE_USERNAME != undefined) {
+      console.log('TRAVIS_JOB_NUMBER: '+process.env.TRAVIS_JOB_NUMBER);
       this.browser = new webdriver.Builder()
       .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
       .withCapabilities({

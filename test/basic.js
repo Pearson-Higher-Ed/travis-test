7df406e2e5a3644500edcc947532fe606897cbb9
Eajaz Ali Mahaboob Basha <eajazali.mahaboobbasha@pearson.com>
2016-02-24 16:34:41 -0700

    test

--- test/basic.js
+++ test/basic.js
@@ -2,7 +2,8 @@ var assert = require("assert");
 var webdriver = require("selenium-webdriver");
 
 describe("testing javascript in the browser", function() {
-  it("should handle clicking on a headline", function(done) {
+  it("should handle clicking on a headline", function(done) {  	
+  	assert.equal(a,b);
     console.log("travis-test-1");
       done();
     });  

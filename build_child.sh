4403520085ca00fe07bffdcbd92c45bc187aacde
Eajaz Ali Mahaboob Basha <eajazali.mahaboobbasha@pearson.com>
2016-02-26 19:16:16 -0700

    “test”

--- build_child.sh
+++ build_child.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 
-url -i -H "Accept: application/vnd.travis-ci.2+json" "https://api.travis-ci.org/repos/Pearson-Higher-Ed/ux-test-platform/builds" > test.json	
+curl -i -H "Accept: application/vnd.travis-ci.2+json" "https://api.travis-ci.org/repos/Pearson-Higher-Ed/ux-test-platform/builds" > test.json	
 LATEST_ID=$(grep -o '"state":.[a-z\"]*' test.json | head -1)
 LATEST_STATE=$(grep -o '"id":.[0-9]*' test.json | head -1 | grep ':.[0-9]*')
 echo "LATEST_ID: $LATEST_ID"
 echo "LATEST_STATE: $LATEST_STATE"
 
+kill -9 $PPID
 echo "whats the exit value:" $?
